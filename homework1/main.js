b730263abd2f59ed5d38b57b040fb51e478e6ee8
MartaFOx <martalesyk59@gmail.com>
2023-08-16 13:44:13 +0300

    add code in main.js

---
+++ homework1/main.js
@@ -0,0 +1,42 @@
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4;
+str4 = 'ua';
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+const PI = 3.14;
+let n5 = 2.7;
+let n6 = 16;
+let b1 = true;
+let b2 = false;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(PI);
+console.log(n5);
+console.log(n6);
+console.log(b1);
+console.log(b2);
+
+let firstName = 'Marta';
+let middleName = 'Romanivna';
+let lastName = 'Lesyk';
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
